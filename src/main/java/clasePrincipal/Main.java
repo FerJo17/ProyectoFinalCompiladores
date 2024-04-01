ed802de1c49f9f9d7af083bad06c99f354287b5d
Ferjo <ferjo17rm@gmail.com>
2024-03-31 22:30:52 -0600

    Inicio del proyecto

---
+++ src/main/java/clasePrincipal/Main.java
@@ -0,0 +1,11 @@
+package clasePrincipal;
+
+import Ventana.ventanaPrograma;
+
+public class Main {
+    public static void main(String[] args) {
+        ventanaPrograma ver = new ventanaPrograma();
+        ver.setVisible(true);
+        ver.setLocationRelativeTo(null);
+    }
+}
