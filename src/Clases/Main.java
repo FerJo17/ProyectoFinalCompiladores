fc6936eaaf96e500dd298242b57660df8769ce40
Ferjo <ferjo17rm@gmail.com>
2024-04-03 22:22:20 -0600

    Cambios

---
+++ src/Clases/Main.java
@@ -0,0 +1,11 @@
+package Clases;
+
+import Frame.Inicio;
+
+public class Main {
+    public static void main(String[] args) {
+        Inicio ver = new Inicio();
+        ver.setVisible(true);
+        ver.setLocationRelativeTo(null);
+    }
+}
